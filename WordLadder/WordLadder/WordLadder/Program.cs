1bf7e3af3ae8816f48455bfcc1f8ca08963812fc
bsleon <bsleon@umich.edu>
2019-08-14 21:36:21 -0400

    add function to determine similar words

--- WordLadder/WordLadder/WordLadder/Program.cs
+++ WordLadder/WordLadder/WordLadder/Program.cs
@@ -6,16 +6,48 @@ using System.Threading.Tasks;
 
 namespace WordLadder
 {
+    public class Solution
+    {
+        public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+        {
+            foreach (var word in wordList)
+            {
+                //Console.WriteLine(word);
+                if (IsSimilarString(word, beginWord))
+                    Console.WriteLine(beginWord + " and " + word + " are similar!");
+            }
+
+
+            return 1;
+        }
+
+        public bool IsSimilarString(string str1, string str2)
+        {
+            int count = 0;
+            int length = str1.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                if (count > 1) return false;
+                if (str1[i] != str2[i]) ++count;
+            }
+
+            return true;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string beginWord = "hit";
+            string endWord = "cog";
+            IList<string> wordList = new List<string> { "hot", "dot", "dog", "lot", "log", "cog", "lit", "him" };
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var solution = new Solution();
+            solution.LadderLength(beginWord, endWord, wordList);
+
+            Console.Read();
+
         }
     }
 }
